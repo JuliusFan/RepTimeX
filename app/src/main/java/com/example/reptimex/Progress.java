ad9cf9f4d25d61a706b6448313582be1a4412e1f
danhuynh1 <danh8953@gmail.com>
2019-10-31 16:03:34 -0400

    Working on Functionality
    Progress View pulls exerciseArray from SharedPreferences.
    
    Currently displays as listview.
    
    CountdownTimer added; hardcoded as 10 minutes.
    
    To do: get duration of exerciseArray's current item and set as CountdownTimer.
    iterate through exerciseArray until completion and updating values.

--- app/src/main/java/com/example/reptimex/Progress.java
+++ app/src/main/java/com/example/reptimex/Progress.java
@@ -3,14 +3,28 @@ package com.example.reptimex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class Progress extends AppCompatActivity {
 
@@ -21,6 +35,35 @@ public class Progress extends AppCompatActivity {
     long startTime;
     long originalStartTime;
     ProgressBar progressBar;
+
+
+
+    //supposed to be current exercise, then when on to next exercise, increments by 1 until exerciseArray,size();
+    int current_index = 0;
+    private TextView countDownText;
+    private Button countdownButton;
+
+
+
+    private TextView currentexercise;
+    private TextView nextexercise;
+    private CountDownTimer stopwatch;
+
+
+    //hard coded 10 minutes
+    private long timeleftmilliseconds = 600000;
+
+   ExerciseAdapter globalAdapter;
+
+
+
+    private boolean isTimerRunning;
+
+    protected final static String DATA_KEY = "exerciseArray";
+    ArrayList<Exercise> exerciseArray;
+    //CreateRoutineActivity.ExerciseAdapter globalAdapter;
+
+
 
     @SuppressLint({"SetTextI18n", "DefaultLocale"})
     @Override
@@ -33,6 +76,36 @@ public class Progress extends AppCompatActivity {
         createRoutine = findViewById(R.id.createRoutine);
         cancelTraining = findViewById(R.id.cancelTraining);
         progressBar = findViewById(R.id.progressBar);
+
+
+        currentexercise=findViewById(R.id.current_exercise);
+        nextexercise = findViewById(R.id.next_exercise);
+
+        countDownText = findViewById(R.id.countdown);
+        countdownButton = findViewById(R.id.countdown_button);
+
+
+
+
+        loadData();
+        currentexercise.setText("Current Exercise: "+ exerciseArray.get(current_index).toString());
+        nextexercise.setText("Next Exercise: "+ exerciseArray.get(current_index+1).toString());
+
+
+        final ListView list = findViewById(R.id.exercise_list_main);
+        //list.setOnItemClickListener(this);
+        final ExerciseAdapter exerciseAdapter = new ExerciseAdapter(this);
+        this.globalAdapter = exerciseAdapter;
+        list.setAdapter(exerciseAdapter);
+
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
+
 
         timer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +162,10 @@ public class Progress extends AppCompatActivity {
                 timer.setText(String.format("%02d:%02d:%03d", minutes, seconds, showMils));
             }
         });
+
+
+
+
     }
 
     Handler timerHandler = new Handler();
@@ -116,10 +193,110 @@ public class Progress extends AppCompatActivity {
         timerHandler.removeCallbacks(timerRunnable);
         startTimer.setText("Start Timer");
     }
+    public void onResume(){
+        super.onResume();
+        loadData();
+
+        final ListView list = findViewById(R.id.exercise_list_main);
+        //list.setOnItemClickListener(this);
+        final ExerciseAdapter exerciseAdapter = new ExerciseAdapter(this);
+        this.globalAdapter = exerciseAdapter;
+        list.setAdapter(exerciseAdapter);
+
+    }
 
     @Override
     public void onStop() {
         super.onStop();
         timerHandler.removeCallbacks(timerRunnable);
     }
+
+
+
+
+
+
+
+
+    //dan
+
+
+    public void startTimer(){
+        stopwatch = new CountDownTimer(timeleftmilliseconds,1000) {
+            @Override
+            public void onTick(long l) {
+                timeleftmilliseconds=l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        countdownButton.setText("PAUSE");
+        isTimerRunning=true;
+    }
+    public void stopTimer(){
+        stopwatch.cancel();
+        isTimerRunning=false;
+        countdownButton.setText("START");
+    }
+
+    //changes the text of the timer textview
+    public void updateTimer(){
+        int minutes = (int) timeleftmilliseconds/60000;
+        int seconds = (int) timeleftmilliseconds%60000/1000;
+
+        String timelefttext = ""+minutes+":";
+        if (seconds<10)timelefttext+="0" ;
+        timelefttext+=seconds;
+        countDownText.setText(timelefttext);
+    }
+    //pause function for timer
+    public void startStop() {
+        if (isTimerRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+
+    }
+
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedpref",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(DATA_KEY,null);
+        Type type = new TypeToken<ArrayList<Exercise>>() {}.getType();
+        this.exerciseArray = gson.fromJson(json, type);
+        if (this.exerciseArray == null)
+            this.exerciseArray = new ArrayList<>();
+    }
+
+
+    private class ExerciseAdapter extends ArrayAdapter<String> {
+
+        public ExerciseAdapter(Context context){
+            super(context, 0);
+        }
+
+        public int getCount(){
+            return exerciseArray.size();
+        }
+
+        public String getItem(int position){
+            return exerciseArray.get(position).toString();
+        }
+
+        public View getView(int position, View convertView, ViewGroup Parent){
+            LayoutInflater inflater = Progress.this.getLayoutInflater();
+            View v = inflater.inflate(R.layout.exercise_item,null);
+            TextView name = v.findViewById(R.id.exercise_item_text);
+            name.setText(getItem(position));
+            return v;
+        }
+
+    }
+
+
 }
